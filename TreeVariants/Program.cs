5144e2a0e6ecc079e39c5eb1934f54e1d2ad6300
Robert Simiyu <salmonrobertsimiyu@gmail.com>
2021-09-15 09:34:16 +0300

    Initialization

---
+++ TreeVariants/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TreeVariants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TreeVariants/Tree.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeVariants
+{
+    class Tree<T>
+    {
+        public TreeNode<T> Root { get; set; }
+    }
+}
